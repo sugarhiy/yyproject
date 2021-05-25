e0690e9ce49e3c455d920343699850ac525ce59e
“sugarhiy” <1647927254@qq.com>
2021-05-25 18:09:23 +0800

    "first commit_01"

--- gameround/gameround_2.py
+++ gameround/gameround_2.py
@@ -8,26 +8,26 @@ my_hp = hp - enemy_power
 enemy_final_hp = enemy_hp - my_power
 谁的hp先为0，那么谁就输了
 '''
-def fight():
-    my_hp=1000
-    my_power=899
-    empty_hp=1000
-    empty_power=200
+def fight():#定义一个打斗的方法
+    my_hp=1000#我的初始血量
+    my_power=899#我的初始攻击力
+    empty_hp=1000#敌人的初始血量
+    empty_power=200#敌人的初始攻击力
     # 对打多轮，谁的血量先小于等于0，谁就输了
     while True:
-        my_hp=my_hp-empty_power
-        empty_hp=empty_hp-my_power
+        my_hp=my_hp-empty_power#我的血量等于我的血量减去敌人的初始攻击力
+        empty_hp=empty_hp-my_power#敌人的血量等于敌人的血量减去我的初始攻击力
 
-        if my_hp<=0:
+        if my_hp<=0:#如果我的血量小于0时
             # pycharm 快捷键， ctrl+D 可以复制当前行
             print("我的剩余血量️",my_hp)
             print("你的剩余血量",empty_hp)
-            print("我输了️")
+            print("我输了️")#输出我输了
             break
-        elif empty_hp<=0:
+        elif empty_hp<=0:#如果敌人的血量小于0时
             print("我的剩余血量️", my_hp)
             print("你的剩余血量", empty_hp)
-            print("你输了")
+            print("你输了")#输出敌人你输了
             break
 
 #调用fight函数
