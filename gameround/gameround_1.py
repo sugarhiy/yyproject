e15a69f460c9b0ae38970db159193b77f95b3851
“sugarhiy” <1647927254@qq.com>
2021-05-25 17:33:00 +0800

    "first commit_01"

--- gameround/gameround_1.py
+++ gameround/gameround_1.py
@@ -1,6 +1,15 @@
 #程序媛：JY子
-
+'''
+一个回合制游戏，每个角色都有hp 和power，
+hp代表血量，power代表攻击力，hp的初始值为1000，
+power的初始值为200。打斗多个回合
+定义一个fight方法：
+my_hp = hp - enemy_power
+enemy_final_hp = enemy_hp - my_power
+谁的hp先为0，那么谁就输了
+'''
 def fight():
+    # 定义四个变量，存放你和我的 血量还有攻击力
     my_hp=100
     my_power=200
     empty_hp=10
--- gameround/gameround_2.py
+++ gameround/gameround_2.py
@@ -1,16 +1,25 @@
 #程序媛：JY子
-
+'''
+一个回合制游戏，每个角色都有hp 和power，
+hp代表血量，power代表攻击力，hp的初始值为1000，
+power的初始值为200。打斗多个回合
+定义一个fight方法：
+my_hp = hp - enemy_power
+enemy_final_hp = enemy_hp - my_power
+谁的hp先为0，那么谁就输了
+'''
 def fight():
     my_hp=1000
     my_power=899
     empty_hp=1000
     empty_power=200
-#对打多伦，谁血量少谁输
+    # 对打多轮，谁的血量先小于等于0，谁就输了
     while True:
         my_hp=my_hp-empty_power
         empty_hp=empty_hp-my_power
 
         if my_hp<=0:
+            # pycharm 快捷键， ctrl+D 可以复制当前行
             print("我的剩余血量️",my_hp)
             print("你的剩余血量",empty_hp)
             print("我输了️")
